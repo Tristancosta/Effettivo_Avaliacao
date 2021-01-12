42b6f6602037955f0aa20b7349388c47ed1301f2
Tristan <tristancosta0307@gmail.com>
2021-01-12 19:37:21 -0300

    primeiro commit

---
+++ Effettivo_Avaliacao/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Effettivo_Avaliacao
+{
+    public class Conexao
+    {
+        public static string StringConexao { get; set; }
+        static Conexao()
+        {
+            Conexao.StringConexao = "Data Source=APOLO;Initial Catalog=Avaliacao;Persist Security Info=True;User ID=avaliacao;Password=white_02";
+        }
+    }
+}
